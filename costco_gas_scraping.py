0bb682c65f9f69e061da3849363acabd6d9ce0d9
phucnguyen02 <ndhp02@gmail.com>
2023-07-09 23:28:21 -0700

    BeautifulSoup instead of Selenium

--- costco_gas_scraping.py
+++ costco_gas_scraping.py
@@ -1,47 +1,22 @@
-from selenium import webdriver
-import time
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import requests
+HEADERS = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0' }
+urls = ["https://www.costco.com/warehouse-locations/tustin-ranch-tustin-ca-122.html",
+        "https://www.costco.com/warehouse-locations/garden-grove-ca-126.html",
+        "https://www.costco.com/warehouse-locations/tustin-ca-1001.html",
+        "https://www.costco.com/warehouse-locations/fullerton-ca-418.html",
+        "https://www.costco.com/warehouse-locations/laguna-niguel-ca-28.html",
+        "https://www.costco.com/warehouse-locations/laguna-marketplace-laguna-niguel-ca-690.html",
+        "https://www.costco.com/warehouse-locations/huntington-beach-ca-1110.html",
+        "https://www.costco.com/warehouse-locations/fountain-valley-ca-411.html",
+        "https://www.costco.com/warehouse-locations/irvine-ca-454.html"]
 
-driver = webdriver.Chrome("chromedriver")
-wait = WebDriverWait(driver, 10)
-location = "Irvine, CA"
-
-driver.get("https://www.costco.com/warehouse-locations?langId=-1&storeId=10301&catalogId=10701")
-#wait.until(EC.visibility_of_element_located((By.ID, "search-warehouse")))
-time.sleep(3)
-
-#Send location to website
-input_box = driver.find_element("id", "search-warehouse")
-input_box.click()
-input_box.clear()
-input_box.send_keys(location)
-input_box.send_keys(Keys.ENTER)
-#wait.until(EC.visibility_of_element_located((By.ID, "warehouse-list")))
-time.sleep(3)
-
-#Retrieve the URLs to all the stores within Irvine, CA
-urls = set()
-for link in driver.find_elements(By.TAG_NAME, "a"):
-    string_link = link.get_attribute("href")
-    if string_link and string_link.startswith("https://www.costco.com/warehouse-locations/"):
-        urls.add(string_link)
-
-#Visit all the URLs
-f = open("output.txt", "w")
 for url in urls:
-    driver.get(url)
-    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "warehouse-body")))
-
-    #Check to see if the store has a gas station
-    if driver.find_elements("xpath", '/html/body/main/div[3]/div[3]/div[3]/div[2]/div/div/div[2]/div[2]/div/div[5]/div[1]/span[2]'):
-        warehouse_name = driver.find_element("xpath", '//*[@id="warehouse"]/div[3]/div[2]/h1').text
-        warehouse_address = driver.find_element("xpath", '//*[@id="address"]/div[1]/span[1]').text
-        regular_gas = driver.find_element("xpath", '/html/body/main/div[3]/div[3]/div[3]/div[2]/div/div/div[2]/div[2]/div/div[5]/div[1]/span[2]').get_attribute('innerHTML').split('<')[0]
-        premium_gas = driver.find_element("xpath", '/html/body/main/div[3]/div[3]/div[3]/div[2]/div/div/div[2]/div[2]/div/div[5]/div[2]/span[2]').get_attribute('innerHTML').split('<')[0]
-        f.write("Name: " + warehouse_name + ". Address: " + warehouse_address + ". Regular Gas: " + regular_gas + ". Premium Gas: " + premium_gas + "\n")
-time.sleep(5)
-f.close()
-driver.close()
+    r = requests.get(url, timeout = 5, headers = HEADERS)
+    soup = BeautifulSoup(r.text, "html.parser")
+    prices = soup.find_all("span", {"class": "gas-type"})
+    name = soup.find("div", {"class": "warehouse-name"}).find("h1").text
+    address = soup.find("span", {"id": "address"}).find("span").text
+    regular_gas = prices[0].parent.find_all("span")[1].text[:-1]
+    premium_gas = prices[1].parent.find_all("span")[1].text[:-1]
+    print("Name: " + name + ". Address: " + address + ". Regular Gas: " + regular_gas + ". Premium Gas: " + premium_gas + "\n")
---
+++ find_irvine_stations.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome("chromedriver")
+wait = WebDriverWait(driver, 10)
+location = "Irvine, CA"
+
+driver.get("https://www.costco.com/warehouse-locations?langId=-1&storeId=10301&catalogId=10701")
+time.sleep(3)
+
+#Send location to website
+input_box = driver.find_element("id", "search-warehouse")
+input_box.click()
+input_box.clear()
+input_box.send_keys(location)
+input_box.send_keys(Keys.ENTER)
+time.sleep(3)
+
+#Retrieve the URLs to all the stores within Irvine, CA
+urls = set()
+for link in driver.find_elements(By.TAG_NAME, "a"):
+    string_link = link.get_attribute("href")
+    if string_link and string_link.startswith("https://www.costco.com/warehouse-locations/"):
+        urls.add(string_link)
+
+driver.close()
