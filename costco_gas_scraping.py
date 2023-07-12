6a5d36658beb770ba05abd24c426e479b743e902
phucnguyen02 <ndhp02@gmail.com>
2023-07-11 20:12:02 -0700

    Firestore connection

--- costco_gas_scraping.py
+++ costco_gas_scraping.py
@@ -1,6 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 HEADERS = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0' }
+
 urls = ["https://www.costco.com/warehouse-locations/tustin-ranch-tustin-ca-122.html",
         "https://www.costco.com/warehouse-locations/garden-grove-ca-126.html",
         "https://www.costco.com/warehouse-locations/tustin-ca-1001.html",
@@ -11,16 +21,28 @@ urls = ["https://www.costco.com/warehouse-locations/tustin-ranch-tustin-ca-122.h
         "https://www.costco.com/warehouse-locations/fountain-valley-ca-411.html",
         "https://www.costco.com/warehouse-locations/irvine-ca-454.html"]
 
-f = open("output.txt", "w")
 
-for url in urls:
-    r = requests.get(url, timeout = 5, headers = HEADERS)
-    soup = BeautifulSoup(r.text, "html.parser")
-    prices = soup.find_all("span", {"class": "gas-type"})
-    name = soup.find("div", {"class": "warehouse-name"}).find("h1").text
-    address = soup.find("span", {"id": "address"}).find("span").text
-    regular_gas = prices[0].parent.find_all("span")[1].text[:-1]
-    premium_gas = prices[1].parent.find_all("span")[1].text[:-1]
-    f.write("Name: " + name + ". Address: " + address + ". Regular Gas: " + regular_gas + ". Premium Gas: " + premium_gas + "\n")
+db.collection('warehouses')
 
-f.close()
+
+def scrape_gas(urls):
+    f = open("output.txt", "w")
+    for url in urls:
+        r = requests.get(url, timeout = 5, headers = HEADERS)
+        soup = BeautifulSoup(r.text, "html.parser")
+        prices = soup.find_all("span", {"class": "gas-type"})
+        name = soup.find("div", {"class": "warehouse-name"}).find("h1").text
+        address = soup.find("span", {"id": "address"}).find("span").text
+        regular_gas = prices[0].parent.find_all("span")[1].text[:-1]
+        premium_gas = prices[1].parent.find_all("span")[1].text[:-1]
+        f.write("Name: " + name + ". Address: " + address + ". Regular Gas: " + regular_gas + ". Premium Gas: " + premium_gas + "\n")
+        data = {'Name': name, 'Address': address, 'Regular_Gas': regular_gas, 'Premium_Gas': premium_gas}
+        available = db.collection('warehouses').document(name).get()
+        if available.exists:
+            db.collection('warehouses').document(name).update(data)
+        else:
+            db.collection('warehouses').document(name).set(data)
+    f.close()
+
+
+scrape_gas(urls)
