80d51e5907ed8a62271180714eadfac9a87a0dd7
phucnguyen02 <ndhp02@gmail.com>
2024-07-13 23:06:26 -0700

    test

--- car_brand_split.py
+++ car_brand_split.py
@@ -7,6 +7,7 @@ from firebase_admin import firestore
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
+#hi
 #Initializing the Firestore client
 db = firestore.client()
 
